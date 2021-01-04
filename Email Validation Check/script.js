9e4ebee57e67016bcbf7a3fc15bc6b35188157e6
Seokyoung Kang <51039577+mathtkang@users.noreply.github.com>
2021-01-05 08:26:19 +0900

    Add files via upload

--- Email Validation Check/script.js
+++ Email Validation Check/script.js
@@ -2,23 +2,25 @@ function validation(){
     var form = document.getElementById("form");
     var email = document.getElementById("email").value;
     var text = document.getElementById("text");
-    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //이메일형식 문자열
 
-    if(email.match(pattern)){
-        form.classList.add("valid");
+//.match() : 문자열 비교
+    if(email.match(pattern)){//일치
+        form.classList.add("valid"); //<form id="form" class="valid">
         form.classList.remove("invalid");
-        text.innerHTML = "Your Email Address in Valid";
-        text.style.color = "#00ff00";
-    }else{
-        form.classList.remove("valid");
+        text.innerHTML = "Your Email Address in Valid";        
+        text.style.color = "#00ff00";//초록색(class="valid")
+
+    }else{//불일치
         form.classList.add("invalid");
+        form.classList.remove("valid");
         text.innerHTML = "Please Enter Valid Email Address";
-        text.style.color = "#ff0000";
+        text.style.color = "#ff0000";//빨간색(class="invalid")
     }
-    if(email == ""){
+    if(email == ""){//빈칸
         form.classList.remove("valid");
         form.classList.remove("invalid");
         text.innerHTML = "";
-        text.style.color = "#00ff00";
+        // text.style.color = "#00ff00";//초록색
     }
 }
